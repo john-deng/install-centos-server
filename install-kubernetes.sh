597c09dd4fde8838333d968d8388d1b51e750f03
John Deng <deng.binghan@vpclub.cn>
2016-10-23 12:14:00 +0800

    set quiet installation mode for kubernetes

--- install-kubernetes.sh
+++ install-kubernetes.sh
@@ -10,7 +10,7 @@ enabled=1
 gpgcheck=0
 EOF
 
-yum install kubelet kubeadm kubectl kubernetes-cni
+yum -y install kubelet kubeadm kubectl kubernetes-cni
 systemctl enable kubelet && systemctl start kubelet
 
 if [ "$1" == "master" ]; then
